c78ebba91da1f9d28be341a29dab67fd9c4c1cd5
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-05-11 17:22:31 -0500

    Login & Form contidional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Login from "./Login";
 import Input from "./Input";
+import Form from "./Form";
 
 var isLoggedIn = true;
+var userIsRegistered = true;
 
 function App() {
-  return <div className="container">{isLoggedIn ? <Input /> : <Login />}</div>;
+  return (
+    <div className="container">{userIsRegistered ? <Login /> : <Form />}</div>
+  );
 }
 
 export default App;
